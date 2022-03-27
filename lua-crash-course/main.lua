9026bf4733abcb455fb6686960b45c324e56e0c3
ggarber42 <ggarber@protonmail.com>
2022-03-27 19:49:41 -0300

    crash course added

---
+++ lua-crash-course/main.lua
@@ -0,0 +1,54 @@
+/*
+tipos de variaveis
+nil -> tipo null
+boolean
+string
+number
+*/
+
+x = 'cool'
+y = true
+z = false
+w = 100
+p = nil
+
+/* 
+table - estrutura de dados do lua
+*/
+
+ourTable = {
+     coolVar = 10,
+     coolTable = {}
+}
+
+ourTable.power = 
+
+/* 
+Relational operators
+==
+~=
+<
+>
+<=
+=>
+and
+or
+elseif
+*/
+money = 150
+if money > 100 and money < 200 then
+     print(money)
+elseif money <= 100
+     print('sad')
+else
+     print('kkkk')
+end
+
+/*
+function
+*/
+
+function printador()
+     print('printador')
+end
+
